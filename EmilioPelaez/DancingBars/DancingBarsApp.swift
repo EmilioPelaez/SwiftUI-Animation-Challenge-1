6bf094f8f5f5b63ae6f7ff61656a64ea2a243a0f
Emilio Peláez <me@epelaez.net>
2022-07-16 14:49:22 +0200

    Create Project

---
+++ EmilioPelaez/DancingBars/DancingBarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DancingBarsApp.swift
+//  DancingBars
+//
+//  Created by Emilio Peláez on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct DancingBarsApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
